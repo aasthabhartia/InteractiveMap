8fa75787a6eb8d696f2fe26e9734734109248ae1
aasthabhartia <aastha.bhartia@gmail.com>
2015-11-08 14:49:55 -0800

    Initial Commit

---
+++ InteractiveMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InteractiveMap
+//
+//  Created by Aastha Bhartia on 11/8/15.
+//  Copyright (c) 2015 Aastha Bhartia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
